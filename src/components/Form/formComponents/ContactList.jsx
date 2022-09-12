dfca14d6c5aae57b9593d41e154cb33b6e9e2d27
Andrii Klochkov <lord@MacBook-Pro-Andrii.local>
2022-09-12 14:23:51 +0300

    fix(filter)

--- src/components/Form/formComponents/ContactList.jsx
+++ src/components/Form/formComponents/ContactList.jsx
@@ -10,38 +10,41 @@ import s from 'components/Form/form.module.scss';
 export default function ContactList() {
   const dispatch = useDispatch();
   const currentContacts = useSelector(getContactsApi);
-  const {loading} = useSelector(getStateContacts);
+  const { loading } = useSelector(getStateContacts);
   const filterWord = useSelector(state => state.filter);
-  const filteredContacts = currentContacts?.filter(el =>
-    el.name.toLowerCase().includes(filterWord.toLowerCase())
+  const filteredContacts = currentContacts?.filter(
+    el =>
+      el.name.toLowerCase().includes(filterWord.toLowerCase()) ||
+      el.phone.includes(filterWord)
   );
 
   useEffect(() => {
-    dispatch(fetchContacts())
+    dispatch(fetchContacts());
   }, [dispatch]);
 
   const deleteContact = id => {
     dispatch(removeContact(id));
   };
 
-  return (
+  return (`
     <div>
       <h2>Contacts</h2>
       <ul>
-        {!loading && filteredContacts?.map(({ id, name, phone }) => {
-          return (
-            <li key={nanoid()} id={id} className={s.item}>
-              <div>{`${name}: ${phone}`}</div>
-              <button
-                className={s.deleteBtn}
-                type="button"
-                onClick={() => deleteContact(id)}
-              >
-                delete
-              </button>
-            </li>
-          );
-        })}
+        {!loading &&
+          filteredContacts?.map(({ id, name, phone }) => {
+            return (
+              <li key={nanoid()} id={id} className={s.item}>
+                <div>{`${name}: ${phone}`}</div>
+                <button
+                  className={s.deleteBtn}
+                  type="button"
+                  onClick={() => deleteContact(id)}
+                >
+                  delete
+                </button>
+              </li>
+            );
+          })}
       </ul>
       {loading && <p>Loading contacts...</p>}
     </div>
